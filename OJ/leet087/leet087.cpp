05def0fd2a7414b0157b7901fc150f714e17a565
Jing <im.hj@qq.com>
2021-04-16 23:47:13 +0800

    update 087

--- OJ/leet087/leet087.cpp
+++ OJ/leet087/leet087.cpp
@@ -11,6 +11,7 @@ leet087
 #include <queue>
 #include <stack>
 #include <string>
+#include <cstring>
 #include <set>
 #include <map>
 #include <unordered_set>
@@ -25,15 +26,56 @@ leet087
 
 using namespace std;
 
-class Solution
-{
-    
-};
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        int M[30][30][31]{};
+        int freq[26]{};
+        auto notOk = [&](int i1, int i2, int len) {
+            memset(freq, 0, sizeof freq);
+            for (int k = 0; k < len; k++)
+            {
+                freq[s1[i1+k] - 'a']++;
+                freq[s2[i2+k] - 'a']--;
+            }
+            return any_of(freq, freq+26, [&](int x) { return x != 0; } );
+        };
 
+        function<bool(int, int, int)> f = [&](int i1, int i2, int len) {
+            if (M[i1][i2][len]) return M[i1][i2][len] == 1;
+            if (notOk(i1, i2, len)) {
+                M[i1][i2][len] = -1;
+                return false;
+            }
+            if (s1.substr(i1, len) == s2.substr(i2, len)) {
+                M[i1][i2][len] = 1;
+                return true;
+            }
+
+            for (int k = 1; k < len; k++)
+            {
+                // 不交换的情况
+                if (f(i1, i2, k) && f(i1 + k, i2 + k, len - k)) {
+                    M[i1][i2][len] = 1;
+                    return true;
+                }
+                // 交换的情况
+                if (f(i1, i2 + len - k, k) && f(i1 + k, i2, len - k)) {
+                    M[i1][i2][len] = 1;
+                    return true;
+                }
+            }
+            M[i1][i2][len] = -1;
+            return false;
+        };
+
+        return f(0, 0, s1.size());
+    }
+};
 
 int main(int argc, char const *argv[])
 {
     Solution sol;   
-    
+    cout << sol.isScramble("eebaacbcbcadaaedceaaacadccd", "eadcaacabaddaceacbceaabeccd") << endl;
     return 0;
 }
--- OJ/leet087/leet087.py
+++ OJ/leet087/leet087.py
@@ -17,8 +17,9 @@ class Solution1:
         return s2 in f(s1)
 
 from collections import Counter
-class Solution:
+class Solution2:
     def isScramble(self, s1: str, s2: str) -> bool:
+        # TLE
         if len(s1) != len(s2): return False
         if s1 == s2: return True
         if Counter(s1) != Counter(s2): return False 
@@ -29,6 +30,28 @@ class Solution:
                 return True
         return False
 
+# 记忆化递归
+from collections import Counter
+M = {}
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2): return False
+        if (s1, s2) in M: return M[(s1, s2)]
+        if s1 == s2: 
+            M[(s1, s2)] = True
+            return True
+        if Counter(s1) != Counter(s2): 
+            M[(s1, s2)] = False
+            return False 
+        for i in range(1, len(s1)):
+            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
+                M[(s1, s2)] = True
+                return True
+            if self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:len(s2)-i]):
+                M[(s1, s2)] = True
+                return True
+        M[(s1, s2)] = False
+        return False
 print(Solution().isScramble(s1 = "great", s2 = "rgeat"))
 print(Solution().isScramble(s1 = "abcde", s2 = "caebd"))
 print(Solution().isScramble(s1 = "a", s2 = "a"))
