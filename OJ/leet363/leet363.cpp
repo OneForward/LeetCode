7b6d0e06bbe9ec0f46f5dc81ff8d0644b9a3bbcf
Jing <im.hj@qq.com>
2021-06-26 02:15:13 +0800

    update leet363

--- OJ/leet363/leet363.cpp
+++ OJ/leet363/leet363.cpp
@@ -1,51 +1,55 @@
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-#include <algorithm>
-#include <utility>
-#include <string>
+/*
+leet363
+
+
+*/
+
+#include "../utils/LeetCpp.utils.hpp"
+
 using namespace std;
+
+#define ALL(A) A.begin(),A.end()
+using VI=vector<int>;
+using VVI=vector<VI>;
+
 
 class Solution {
 public:
-    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
-        if ( k < 0) return k;
+    int maxSumSubmatrix(const vector<vector<int>>& M, int k) {
+        // O(R^2 C log C)
+        int R = M.size(), C = M[0].size();
+        
+        vector f(C, 0); int ans = INT_MIN;
+        for (int r1 = 0; r1 < R; r1++)
+        {
+            fill(f.begin(), f.end(), 0);
+            for (int r2 = r1; r2 < R; r2++)
+            {
 
-        int length = matrix.size() + 1, width = matrix[0].size() + 1;
-        vector<int> A(length*width, 0);
-        
-        for (int i=1; i<length; ++i) {
-            for (int j=1; j<width; ++j) {
-                A[i*width+j] = A[(i-1)*width+j] + A[i*width+j-1] - A[(i-1)*width+j-1] + matrix[i-1][j-1];
+                for (int c = 0; c < C; c++)
+                {
+                    f[c] += M[r2][c];
+                }
+                
+                set S{0}; int sum = 0;
+                for (auto&& x: f) {
+                    sum += x;
+                    auto lft = S.lower_bound(sum - k);
+                    if (lft != S.end()) ans = max(sum - *lft, ans);
+                    S.insert(sum);
+                }
+                if (ans == k) break;
             }
+            
         }
-        sort(A.begin(), A.end());
-        int ans = 0; vector<int>::iterator it = A.end();
-        for (int i=length*width-1; i>=0; --i) {
-            it = lower_bound(A.begin(), it, A[i]-k);
-            if (*it == A[i]-k) return k;
-            else if (ans < A[i] - *it) ans = A[i] - *it;
-        }
-        
         return ans;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    // freopen("leet363/1.in", "r", stdin);
-
-    int length, width;
-    cin >> length >> width;
-    
-    vector<vector<int>> M{{2,2,-1}};
-    int k = 2;
-    
-    Solution solution;
-    int ans = solution.maxSumSubmatrix(M, k);
-
-    printf("ans = %d\n", ans);
-
+    Solution sol;   
+    cout << sol.maxSumSubmatrix({{2,2,-1}}, 2) << endl;
+    cout << sol.maxSumSubmatrix({{1,0,1},{0,-2,3}}, 3) << endl;
     return 0;
-        
 }
